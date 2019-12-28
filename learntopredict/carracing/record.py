6ce4efadb0f45cb43d7aaacf8fbb8fe2ed94e62e
kosuke miyoshi <miyoshi@narr.jp>
2019-12-28 16:52:42 +0900

    concatenate recording images for car racing

--- learntopredict/carracing/record.py
+++ learntopredict/carracing/record.py
@@ -11,23 +11,36 @@ class ImageRecorder:
         
         self.counter = 0
 
+    def concat_images(self, images):
+        """ Concatenate images into one image """
+        image_height = images[0].shape[0]
+        spacer = np.ones([image_height, 1, 3], dtype=np.uint8) * 255
+        images_with_spacers = []
+            
+        image_size = len(images)
+            
+        for i in range(image_size):
+            images_with_spacers.append(images[i])
+            if i != image_size-1:
+                # Add 1 pixel space
+                images_with_spacers.append(spacer)
+        ret = np.hstack(images_with_spacers)
+        return ret
+
     def record(self, img_input, obs_real, obs_pred, peeked, vae):
         img_pred = vae.decode(obs_pred.reshape(1, -1)) * 255.0
         img_pred = np.round(img_pred).astype(np.uint8)
         img_pred = img_pred.reshape(64, 64, 3)
-        imageio.imwrite(os.path.join(self.save_dir,
-                                     "pred_" + (format(self.counter, "05d") + ".png")),
-                        img_pred)
-
+        
         img_dec = vae.decode(obs_real.reshape(1, -1)) * 255.0
         img_dec = np.round(img_dec).astype(np.uint8)
         img_dec = img_dec.reshape(64, 64, 3)
-        imageio.imwrite(os.path.join(self.save_dir,
-                                     "dec_" + (format(self.counter, "05d") + ".png")),
-                        img_dec)
 
         img_input = np.round(img_input * 255.0).astype(np.uint8).reshape(64, 64, 3)
+
+        img_all = self.concat_images([img_input, img_dec, img_pred])
         imageio.imwrite(os.path.join(self.save_dir,
-                                     "input_" + (format(self.counter, "05d") + ".png")),
-                        img_input)
+                                     "" + (format(self.counter, "05d") + ".png")),
+                        img_all)
+        
         self.counter += 1
