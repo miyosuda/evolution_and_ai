98edaa526e13c353f5cad26ccd30b7667f974c4a
kosuke miyoshi <miyoshi@narr.jp>
2020-01-12 15:26:07 +0900

    add multi agent emergence code

---
+++ multi_agent_emergence/run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#python3 bin/examine.py examples/hide_and_seek_quadrant.jsonnet examples/hide_and_seek_quadrant.npz
+
+#python3 bin/examine.py examples/hide_and_seek_full.jsonnet examples/hide_and_seek_full.npz
+#python3 bin/examine.py examples/shelter.jsonnet examples/shelter.npz
+#python3 bin/examine.py examples/blueprint.jsonnet examples/blueprint.npz
+#python3 bin/examine.py examples/lock_and_return.jsonnet examples/lock_and_return.npz
+#python3 bin/examine.py examples/lock_and_return.jsonnet examples/lock_and_return.npz
+
+#python3 bin/examine.py examples/hide_and_seek_quadrant.jsonnet examples/hide_and_seek_quadrant_physics_exploits.npz
+
+#python3 bin/examine.py examples/sequential_lock.jsonnet examples/sequential_lock.npz
+
+
+#python3 bin/examine.py examples/hide_and_seek_full.jsonnet examples/hide_and_seek_policy_phases/a_chasing.npz
+#python3 bin/examine.py examples/hide_and_seek_full.jsonnet examples/hide_and_seek_policy_phases/b_forts.npz
+python3 bin/examine.py examples/hide_and_seek_full.jsonnet examples/hide_and_seek_policy_phases/e_box_surfing.npz
+
